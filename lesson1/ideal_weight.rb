1da1ca6d9df275b4a772e8958c9a07d5227545c8
kurchinm <kurchin.m@yandex.ru>
2022-01-20 18:21:20 +0300

    1

---
+++ lesson1/ideal_weight.rb
@@ -0,0 +1,10 @@
+puts "Input your name?"
+name=gets.chomp
+puts "Input your height?"
+height=gets.to_i
+ideal_weight=(height-110)*1.15
+if ideal_weight<0 
+	puts "Your weight is already optimal"
+else
+	puts "#{name}, your ideal weight is #{ideal_weight}"
+end
