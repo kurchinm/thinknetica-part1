2f7b0f4c6b13da97f9d17cace511cb6b5bf5f80a
kurchinm <kurchin.m@yandex.ru>
2022-03-18 16:09:01 +0300

    lesson9

---
+++ lesson9/main.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# lesson 4
+
+require_relative 'menu'
+
+menu = Menu.new
+menu.main
---
+++ lesson9/route.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Route
+  include InstanceCounter
+
+  FORMAT_NAME = /\S{7,}/.freeze
+
+  attr_reader :station_list, :name
+
+  def initialize(start_station, stop_station, name)
+    @name = name
+    validate!
+    @station_list = [start_station, stop_station]
+  end
+
+  def add_station(station)
+    @station_list.insert(-2, station) if @station_list.include?(station) == false
+  end
+
+  def del_station(station)
+    @station_list.delete(station) if @station_list.include?(station) == true
+  end
+
+  def valid?
+    validate!
+    true
+  rescue StandardError
+    false
+  end
+
+  private
+
+  def validate!
+    raise 'Invalid route name format!' if name !~ FORMAT_NAME
+  end
+end
